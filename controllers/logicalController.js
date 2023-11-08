0600ee952f2f45396d09f5c0df815277012c51f4
MannShah17 <mannshah2004@gmail.com>
2023-11-08 07:27:09 +0530

    done

--- controllers/logicalController.js
+++ controllers/logicalController.js
@@ -1,55 +1,44 @@
 const router = require('express').Router();
 const Product = require('../models/logicalModel');
 
-//Get all products where price is 2000 or name is 'product1'
+// TODO: Implement logical operators for querying products
+
+// Get all products where price is 2000 or name is 'product1'
 const or = async (req, res) => {
   try {
-    const products = await Product.find({
-      $or: [{ price: 2000 }, { name: 'product1' }],
-    });
-    console.log(products);
-    res.status(200).json(products);
+    // TODO: Write a query to find products matching the given conditions using the $or operator
+    // TODO: Use $or to find products where price is 2000 or name is 'product1'
   } catch (error) {
     res.status(404).send(error);
   }
 };
 
-//Get all products where price is 2000 and name is 'product1'
+// Get all products where price is 2000 and name is 'product1'
 const and = async (req, res) => {
   try {
-    const products = await Product.find({
-      $and: [{ price: 2000 }, { name: 'product1' }],
-    });
-    console.log(products);
-    res.status(200).json(products);
+    // TODO: Write a query to find products matching the given conditions using the $and operator
+    // TODO: Use $and to find products where price is 2000 and name is 'product1'
   } catch (error) {
     res.status(404).send(error);
   }
 };
 
-//Get all products where price is not greater than 2000 (i.e. less than or equal to 2000)
+// Get all products where price is not greater than 2000 (i.e., less than or equal to 2000)
 const not = async (req, res) => {
   try {
-    const products = await Product.find({
-      price: { $not: { $gt: 2000 } },
-    });
-    console.log(products);
-    res.status(200).json(products);
+    // TODO: Write a query to find products where price is not greater than 2000 using the $not and $gt operators
   } catch (error) {
     res.status(404).send(error);
   }
 };
 
-//Get all products except those where price is 2000 or name is 'product1'
+// Get all products except those where price is 2000 or name is 'product1'
 const nor = async (req, res) => {
   try {
-    const products = await Product.find({
-      $nor: [{ price: 2000 }, { name: 'product1' }],
-    });
-    console.log(products);
-    res.status(200).json(products);
+    // TODO: Write a query to find products except those where price is 2000 or name is 'product1' using the $nor operator
   } catch (error) {
     res.status(404).send(error);
   }
 };
+
 module.exports = { or, and, not, nor };
